d1a7c42cf688b7ce33667ffc8d630e1686b09023
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-05-08 23:19:05 -0300

    Hover of questions remove when answer is actived

--- script/script.js
+++ script/script.js
@@ -1,27 +1,30 @@
 // Elements
 
-let questions = document.querySelectorAll('.js-expand')
+let expands = document.querySelectorAll('.js-expand')
 let answers = document.querySelectorAll('.js-group__answer')
 let arrows = document.querySelectorAll('.js-group__arrow')
+let question = document.querySelectorAll('.group__question')
 
 // Event Listener
 
-for (let i = 0; i in questions; i++) {
-    questions[i].addEventListener('click', function() {
-        dropAnswer(questions[i], answers[i], arrows[i] )
+for (let i = 0; i in expands; i++) {
+    expands[i].addEventListener('click', function() {
+        dropAnswer(expands[i], answers[i], arrows[i], question[i] )
     })
 }
 
 // Show de answers 
 
-function dropAnswer(question, answer, arrow) {
+function dropAnswer(expand, answer, arrow, question) {
     if (answer.classList.contains('js-answer-active')) {
-        question.classList.remove('js-question-active')
+        expand.classList.remove('js-expand-active')
         answer.classList.remove('js-answer-active')
         arrow.classList.remove('js-arrow-active')
+        question.classList.add('group__js-question')
     } else {
-        question.classList.add('js-question-active')
+        expand.classList.add('js-expand-active')
         answer.classList.add('js-answer-active')
         arrow.classList.add('js-arrow-active')
+        question.classList.remove('group__js-question')
     }
 }
