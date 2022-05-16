bb43b8e71674ed3a53d6d6ff5877759b1392d081
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-05-16 15:38:28 -0300

    Expand class, substituted to accordion

--- script/script.js
+++ script/script.js
@@ -1,28 +1,28 @@
 // Elements
 
-let expands = document.querySelectorAll('.js-faq-questions__expand')
+let accordions = document.querySelectorAll('.js-faq-questions__accordion')
 let answers = document.querySelectorAll('.js-faq-questions__answer')
 let arrows = document.querySelectorAll('.js-faq__arrow')
 let questions = document.querySelectorAll('.faq-questions__statement')
 
 // Event Listener
 
-for (let i = 0; i in expands; i++) {
-    expands[i].addEventListener('click', function() {
-        answerCutout(expands[i], answers[i], arrows[i], questions[i])
+for (let i = 0; i in accordions; i++) {
+    accordions[i].addEventListener('click', function() {
+        answerCutout(accordions[i], answers[i], arrows[i], questions[i])
     })
 }
 
 // Show de answers 
 
-function answerCutout(expand, answer, arrow, question) {
+function answerCutout(accordion, answer, arrow, question) {
     if (answer.classList.contains('js-answer-active')) {
-        expand.classList.remove('js-faq-questions__expand-active')
+        accordion.classList.remove('js-faq-questions__accordion-active')
         answer.classList.remove('js-answer-active')
         arrow.classList.remove('js-arrow-active')
         question.classList.add('js-faq-questions__statement')
     } else {
-        expand.classList.add('js-faq-questions__expand-active')
+        accordion.classList.add('js-faq-questions__accordion-active')
         answer.classList.add('js-answer-active')
         arrow.classList.add('js-arrow-active')
         question.classList.remove('js-faq-questions__statement')
