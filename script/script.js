83e32912e8c438ce9494d2868e5f155842139a37
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-05-16 13:45:04 -0300

    class doubt changed for questions

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 // Elements
 
-let expands = document.querySelectorAll('.js-doubt__expand')
+let expands = document.querySelectorAll('.js-question__expand')
 let answers = document.querySelectorAll('.js-faq__answer')
 let arrows = document.querySelectorAll('.js-faq__arrow')
 let questions = document.querySelectorAll('.faq__question')
@@ -17,12 +17,12 @@ for (let i = 0; i in expands; i++) {
 
 function answerCutout(expand, answer, arrow, question) {
     if (answer.classList.contains('js-answer-active')) {
-        expand.classList.remove('js-doubt__expand-active')
+        expand.classList.remove('js-question__expand-active')
         answer.classList.remove('js-answer-active')
         arrow.classList.remove('js-arrow-active')
         question.classList.add('faq__js-question')
     } else {
-        expand.classList.add('js-doubt__expand-active')
+        expand.classList.add('js-question__expand-active')
         answer.classList.add('js-answer-active')
         arrow.classList.add('js-arrow-active')
         question.classList.remove('faq__js-question')
