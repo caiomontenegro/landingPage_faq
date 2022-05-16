521938573e0bae8a3796c58cf7461426eb6d098e
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-05-16 15:53:34 -0300

    Js update, now it works with two functions

--- script/script.js
+++ script/script.js
@@ -9,22 +9,26 @@ let questions = document.querySelectorAll('.faq-questions__statement')
 
 for (let i = 0; i in accordions; i++) {
     accordions[i].addEventListener('click', function() {
-        answerCutout(accordions[i], answers[i], arrows[i], questions[i])
+        if (answers[i].classList.contains('js-answer-active')) {
+            retractAnswer(accordions[i], answers[i], arrows[i], questions[i])
+        } else {
+            expandAnswer(accordions[i], answers[i], arrows[i], questions[i])
+        }
     })
 }
 
 // Show de answers 
 
-function answerCutout(accordion, answer, arrow, question) {
-    if (answer.classList.contains('js-answer-active')) {
-        accordion.classList.remove('js-faq-questions__accordion-active')
-        answer.classList.remove('js-answer-active')
-        arrow.classList.remove('js-arrow-active')
-        question.classList.add('js-faq-questions__statement')
-    } else {
-        accordion.classList.add('js-faq-questions__accordion-active')
-        answer.classList.add('js-answer-active')
-        arrow.classList.add('js-arrow-active')
-        question.classList.remove('js-faq-questions__statement')
-    }
+function expandAnswer(accordion, answer, arrow, question) {
+    accordion.classList.add('js-faq-questions__accordion-active')
+    answer.classList.add('js-answer-active')
+    arrow.classList.add('js-arrow-active')
+    question.classList.remove('js-faq-questions__statement')
+}
+
+function retractAnswer(accordion, answer, arrow, question) {
+    accordion.classList.remove('js-faq-questions__accordion-active')
+    answer.classList.remove('js-answer-active')
+    arrow.classList.remove('js-arrow-active')
+    question.classList.add('js-faq-questions__statement')
 }
