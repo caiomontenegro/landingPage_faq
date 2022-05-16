3241be4402a81fe916bf6aaf61301c0c5659b3f2
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-05-16 15:33:37 -0300

    bug of questions titles hover fixed

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
 // Elements
 
 let expands = document.querySelectorAll('.js-faq-questions__expand')
-let answers = document.querySelectorAll('.js-faq__answer')
+let answers = document.querySelectorAll('.js-faq-questions__answer')
 let arrows = document.querySelectorAll('.js-faq__arrow')
-let questions = document.querySelectorAll('.faq__question')
+let questions = document.querySelectorAll('.faq-questions__statement')
 
 // Event Listener
 
@@ -20,11 +20,11 @@ function answerCutout(expand, answer, arrow, question) {
         expand.classList.remove('js-faq-questions__expand-active')
         answer.classList.remove('js-answer-active')
         arrow.classList.remove('js-arrow-active')
-        question.classList.add('faq__js-question')
+        question.classList.add('js-faq-questions__statement')
     } else {
         expand.classList.add('js-faq-questions__expand-active')
         answer.classList.add('js-answer-active')
         arrow.classList.add('js-arrow-active')
-        question.classList.remove('faq__js-question')
+        question.classList.remove('js-faq-questions__statement')
     }
 }
